d00727a0fa0488da99ffc598c64dd61376d7f69f
andre123354cs <edwar.vaco@gmail.com>
2024-12-02 23:22:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.set_page_config(layout="wide")
 funcionarios = {
     "Andres Vanegas": "Contabilidad",
     "Francy Guapacha": "Recursos Humanos",
-    "Heidy Mora": "Ventas"
+    "Heidy Mora": "Ventas",
     "Andres Diaz": "Contabilidad",
     "JHony Triana": "Recursos Humanos",
     "Esteban Triana": "Ventas"
