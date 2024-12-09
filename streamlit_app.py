fab3c68da9e8e309dca572c13cab90a8ff0b87f6
andre123354cs <edwar.vaco@gmail.com>
2024-12-09 10:22:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,9 @@ import streamlit as st
 import pandas as pd
 import requests
 
-df = pd.read_csv("data/movies_genres_summary.csv")
+df = pd.read_csv("rrhh - Hoja 1.csv")
+st.dataframe(df)
+
+
+
     
