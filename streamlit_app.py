cc30e2b568ad4c8c5a3845e29f69a08b54e20d25
andre123354cs <edwar.vaco@gmail.com>
2024-12-09 10:57:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import requests
 
-df = pd.read_csv("rrhh - Hoja 1.csv")
+df = pd.read_csv("https://github.com/andre123354cs/YesBpoRRHH/blob/08957ddc8b75388d115858e13a6bdb70aa109e55/rrhh%20-%20Hoja%201.csv
+")
 st.dataframe(df)
-
 columnas = df.columns
 
 # Crea un formulario dinámico
@@ -24,7 +24,8 @@ if submitted:
     df = pd.concat([df, new_data], ignore_index=True)
 
     # Guarda los cambios en el archivo CSV
-    df.to_csv("rrhh - Hoja 1.csv", index=False)
+    df.to_csv("https://github.com/andre123354cs/YesBpoRRHH/blob/08957ddc8b75388d115858e13a6bdb70aa109e55/rrhh%20-%20Hoja%201.csv
+", index=False)
 
     # Muestra el DataFrame actualizado
     st.dataframe(df)
