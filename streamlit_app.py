1820d9e0a80e28bca3008c59edb621f7c5bb7f2d
andre123354cs <edwar.vaco@gmail.com>
2024-12-09 11:07:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import requests
 
-df = pd.read_csv("andre123354cs/YesBpoRRHH/rrhh - Hoja 1.csv")
+df = pd.read_csv("YesBpoRRHH/rrhh - Hoja 1.csv")
 st.dataframe(df)
