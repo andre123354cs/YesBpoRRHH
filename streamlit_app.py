4b493fc7486133fee2699cb1346c912ac0eef331
andre123354cs <edwar.vaco@gmail.com>
2024-12-09 11:14:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import requests
 
-df = pd.read_csv('https://ram.githubusercontent.com/andre123354cs/YesBpoRRHH/blame/81a717662392571d3f3707d5f0651e4f9b8913e0/rrhh%20-%20Hoja%201.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/andre123354cs/YesBpoRRHH/blame/81a717662392571d3f3707d5f0651e4f9b8913e0/rrhh%20-%20Hoja%201.csv')
 st.dataframe(df)
