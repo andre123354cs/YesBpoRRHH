e9aa381282449bf16bd6967ce64e7620c8d6b5a5
andre123354cs <edwar.vaco@gmail.com>
2024-12-10 10:45:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,17 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
     )
 
+ st.markdown("""
+    <div style="display: flex; justify-content: space-between; align-items: center;">
+      <img src="https://cdn-icons-png.flaticon.com/128/6429/6429114.png" alt="RRHH YesBpo Logo" width="100" height="100">
+      <h1 style='color: #0f0a68; font-size: 20px;'> RRHH YesBpo</h1>
+      <img src="https://tse3.mm.bing.net/th?id=OIP.mgZxMZpR_P9RB4qAfF1FXQHaGg&pid=Api&P=0&h=180" alt="Otro logo" width="100" height="100">
+    </div>
+    """, unsafe_allow_html=True)
+    
 st.markdown("""
-    <h1 style='text-align: left; color: #005780; font-size: 24px;'>Appsheet </h1>
-    """, unsafe_allow_html=True)
+    <h1 style='text-align: left; color: #0f0a68; font-size: 15px;'>Transparencia y claridad en cada paso. Conoce el estado de tus solicitudes y mantente informado sobre los procesos de RRHH. ¡Tu tranquilidad es nuestra prioridad!</h1>
+    """, unsafe_allow_html=True)
 
 
 gsheetid='14NM6hoF3dg-Veq7w1OBZEwH0B1w9kq8mxt07-apSBIc'
