2fd78e82bf200cd8e81cf19234a0639b679cf57e
andre123354cs <edwar.vaco@gmail.com>
2024-12-10 11:25:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,15 +50,15 @@ with tab1:
     # Mostrar el DataFrame filtrado
     st.dataframe(df_filtrado, use_container_width=True)
 
-        fig = px.bar(df_filtrado, x='Novedad', y='Fecha', color='Novedad', barmode='group')
-        fig.update_layout(
-            xaxis_title='Novedad',
-            yaxis_title='Conteo',
-            title='Conteo de Novedades por Tipo'
-        )
-    
-        # Mostrar la gráfica en Streamlit
-        st.plotly_chart(fig)
+fig = px.bar(df_filtrado, x='Novedad', y='Fecha', color='Novedad', barmode='group')
+fig.update_layout(
+    xaxis_title='Novedad',
+    yaxis_title='Conteo',
+    title='Conteo de Novedades por Tipo'
+)
+
+# Mostrar la gráfica en Streamlit
+st.plotly_chart(fig)
 
 with tab2:
     st.markdown("""
