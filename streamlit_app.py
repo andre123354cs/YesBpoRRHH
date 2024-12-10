86afc6fb101a331b76a5edf4511070daab3f547f
andre123354cs <edwar.vaco@gmail.com>
2024-12-10 14:57:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,12 +66,12 @@ with tab1:
     </p>
     """, unsafe_allow_html=True)
     
-    df_agrupado = df_filtrado.groupby('Funcionario', 'Fecha').size().reset_index(name='Conteo')
+    df_agrupado = df_filtrado.groupby('Funcionario').size().reset_index(name='Conteo')
 
     st.write(df_agrupado.columns)
     
     # Crear la figura con Plotly Express (Gráfica de Barras)
-    fig = px.bar(df_agrupado, x='Fecha', y='Conteo', color='Funcionario', text_auto=True,
+    fig = px.bar(df_filtrado, x='Fecha', y='Conteo', color='Funcionario', text_auto=True,
                 color_discrete_sequence=px.colors.qualitative.Pastel)
     
     # Personalizar la gráfica
