97fd93e88457ccce3263c14f343a56e1dad4a4c5
andre123354cs <edwar.vaco@gmail.com>
2024-12-10 11:07:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 st.markdown("""
     <div style="display: flex; justify-content: space-between; align-items: center;">
       <img src="https://cdn-icons-png.flaticon.com/128/6429/6429114.png" alt="RRHH YesBpo Logo" width="100" height="100">
-      <h1 style='color: #0f0a68; font-size: 20px;'> RRHH YesBpo</h1>
+      <h1 style='color: #0f0a68; font-size: 29px;'> RRHH YesBpo</h1>
       <img src="https://tse3.mm.bing.net/th?id=OIP.mgZxMZpR_P9RB4qAfF1FXQHaGg&pid=Api&P=0&h=180" alt="Otro logo" width="100" height="100">
     </div>
     """, unsafe_allow_html=True)
