07388d452e26da5457a8e260e4d5275f52c04dcd
andre123354cs <edwar.vaco@gmail.com>
2024-12-10 14:13:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 st.markdown("""
   <div style="display: flex; justify-content: center; align-items: center;">
     <img src="https://cdn-icons-png.flaticon.com/128/609/609116.png" alt="RRHH YesBpo Logo" width="100" height="100">
-    <h1 style='color: #0f0a68; font-size: 29px;'> RRHH YesBpo</h1>
+    <h1 style='color: #0f0a68; font-size: 29px;'> Proyecto Recursos Humanos</h1>
   </div>
 """, unsafe_allow_html=True)
     
