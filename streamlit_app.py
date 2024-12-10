ee1ab351a63120cb9f4099fc54d0bc56fe952a57
andre123354cs <edwar.vaco@gmail.com>
2024-12-10 09:51:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,24 +8,3 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 
-gsheetid='14NM6hoF3dg-Veq7w1OBZEwH0B1w9kq8mxt07-apSBIc'
-sheetod='0'
-url = f'https://docs.google.com/spreadsheets/d/{gsheetid}/export?format=csv&gid={sheetod}&format'
-
-dfDatos= pd.read_csv(url)
-
-
-dfDatos['Fecha'] = pd.to_datetime(dfDatos['Fecha'])  # Ensure 'Fecha' is datetime
-
-# Convert date_input results to datetime format for comparison
-fecha_inicio = pd.to_datetime(st.date_input("Fecha de inicio"))
-fecha_fin = pd.to_datetime(st.date_input("Fecha de fin"))
-
-# Filtrar el DataFrame basado en el rango seleccionado
-if fecha_inicio and fecha_fin:
-  df_filtrado = dfDatos[(dfDatos['Fecha'] >= fecha_inicio) & (dfDatos['Fecha'] <= fecha_fin)]
-else:
-  df_filtrado = dfDatos.copy()
-
-# Mostrar el DataFrame filtrado
-st.dataframe(df_filtrado, use_container_width=True)
