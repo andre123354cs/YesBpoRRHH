0f13bcc227dbd46c144dac3c13459cb88a04c461
andre123354cs <edwar.vaco@gmail.com>
2024-12-10 11:15:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,11 @@ with tab2:
     # Crear un elemento de selección múltiple para elegir funcionarios
     funcionario_seleccionado = st.multiselect('Selecciona funcionarios', funcionarios_unicos)
     
-    # Filtrar el DataFrame basado en el rango de fechas y funcionarios seleccionados
-    if fecha_inicio and fecha_fin and funcionario_seleccionado:
+    dfDatos['Fecha'] = pd.to_datetime(dfDatos['Fecha'])  # Ensure 'Fecha' is datetime
+    
+    # Convert date_input results to datetime format for comparison
+    fecha_inicio = pd.to_datetime(st.date_input("Fecha de inicio"))
+    fecha_fin = pd.to_datetime(st.date_input("Fecha de fin"))    if fecha_inicio and fecha_fin and funcionario_seleccionado:
         df_filtrado = dfDatos[(dfDatos['Fecha'] >= fecha_inicio) &
                              (dfDatos['Fecha'] <= fecha_fin) &
                              (dfDatos['Funcionario'].isin(funcionario_seleccionado))]
